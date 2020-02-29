6e4f17569ca9b6682671780538c239410abf0630
Peter Anderson <pete.anderson@gmail.com>
2020-02-29 20:44:05 +0100

    improved slider

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 var blocks = Array.from(document.getElementsByClassName("block"));
+var marker = document.querySelector('.marker');
+var bar = document.querySelector('.bar');
+var sliderDistanceFromTop = document.querySelector('.slider').getBoundingClientRect().top
 
 blocks.forEach(block => {
     block.addEventListener("click", selectBlock);
 });
+
+marker.addEventListener('mousedown', function() {
+    document.addEventListener('mousemove', handleMouseMove);
+});
+
+document.addEventListener('mouseup', function() {
+    document.removeEventListener("mousemove", handleMouseMove, false);
+});
+
+function handleMouseMove(e) {
+    var distance = e.clientY - 10 - sliderDistanceFromTop;
+    if (distance > -10 && distance < 390) {
+        marker.style.top = distance + 'px';
+        bar.style.top = distance + 'px';
+        bar.style.height = 410 - e.clientY + sliderDistanceFromTop + 'px';
+    }
+    
+}
 
 function selectBlock(e) {
     blocks.forEach(block => {
