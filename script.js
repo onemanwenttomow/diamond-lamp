eb49907cb6b5422a78b30287c0f25be6cfc7a5ac
Peter Anderson <pete.anderson@gmail.com>
2020-03-01 14:05:28 +0100

    Can now change brightness of lamp

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 var blocks = Array.from(document.getElementsByClassName("block"));
 var marker = document.querySelector('.marker');
 var bar = document.querySelector('.bar');
-var sliderDistanceFromTop = document.querySelector('.slider').getBoundingClientRect().top
+var sliderDistanceFromTop = document.querySelector('.slider').getBoundingClientRect().top;
+var selected
+var userSelectedOpacity = 0.7;
 
 blocks.forEach(block => {
     block.addEventListener("click", selectBlock);
 });
 
-marker.addEventListener('mousedown', function() {
+marker.addEventListener('mousedown', function(e) {
+    e.preventDefault();
     document.addEventListener('mousemove', handleMouseMove);
 });
 
@@ -16,11 +19,15 @@ document.addEventListener('mouseup', function() {
 });
 
 function handleMouseMove(e) {
+    e.preventDefault();
     var distance = e.clientY - 10 - sliderDistanceFromTop;
-    if (distance > -10 && distance < 390) {
+    selected = document.querySelector('.selected');
+    userSelectedOpacity = ((400 - (distance + 10)) / 400) + 0.2;
+    if (distance >= -10 && distance <= 390) {
         marker.style.top = distance + 'px';
         bar.style.top = distance + 'px';
         bar.style.height = 410 - e.clientY + sliderDistanceFromTop + 'px';
+        selected.style.opacity = userSelectedOpacity;
     }
     
 }
@@ -28,6 +35,8 @@ function handleMouseMove(e) {
 function selectBlock(e) {
     blocks.forEach(block => {
         block.classList.remove("selected");
+        block.style.opacity = 0.2;
     });
-    e.target.classList.add("selected");
+    e.target.style.opacity = userSelectedOpacity;
+    e.target.classList.add('selected');
 }
