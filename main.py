e9b37c250d12a60e41a735ad0c0cbde4d4f383ed
Peter Anderson <pete.anderson@gmail.com>
2020-03-01 21:03:33 +0100

    working flask server

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
-from flask import Flask, render_template      
+from flask import Flask, jsonify, render_template, request
+from flask_cors import CORS
+
+# configuration
+# DEBUG = True
+
+# instantiate the app
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+# sanity check route
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('pong!')
 
 
 @app.route('/')
 def home():
     return render_template("home.html")
 
+
+@app.route('/switch-colour', methods=['POST'])
+def postmethod():
+    post_data = request.get_json()
+    print("post_data: ", post_data)
+    return jsonify({"success": "true"})
+
+
+@app.route('/switch-brightness', methods=['POST'])
+def postmethodbright():
+    post_data = request.get_json()
+    print("post_data: ", post_data)
+    return jsonify({"success": "true"})
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- static/script.js
+++ static/script.js
@@ -1,35 +1,41 @@
 var blocks = Array.from(document.getElementsByClassName("block"));
-var marker = document.querySelector('.marker');
-var bar = document.querySelector('.bar');
-var sliderDistanceFromTop = document.querySelector('.slider').getBoundingClientRect().top;
-var selected
+var marker = document.querySelector(".marker");
+var bar = document.querySelector(".bar");
+var slider = document.querySelector(".slider");
+var sliderDistanceFromTop = slider.getBoundingClientRect().top;
+var selected;
 var userSelectedOpacity = 0.7;
 
 blocks.forEach(block => {
     block.addEventListener("click", selectBlock);
 });
 
-marker.addEventListener('mousedown', function(e) {
+marker.addEventListener("mousedown", function(e) {
     e.preventDefault();
-    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener("mousemove", handleMouseMove);
 });
 
-document.addEventListener('mouseup', function() {
+document.addEventListener("mouseup", function() {
     document.removeEventListener("mousemove", handleMouseMove, false);
 });
 
 function handleMouseMove(e) {
     e.preventDefault();
     var distance = e.clientY - 10 - sliderDistanceFromTop;
-    selected = document.querySelector('.selected');
-    userSelectedOpacity = ((400 - (distance + 10)) / 400) + 0.2;
+    selected = document.querySelector(".selected");
+    userSelectedOpacity = (400 - (distance + 10)) / 400 + 0.2;
     if (distance >= -10 && distance <= 390) {
-        marker.style.top = distance + 'px';
-        bar.style.top = distance + 'px';
-        bar.style.height = 410 - e.clientY + sliderDistanceFromTop + 'px';
+        marker.style.top = distance + "px";
+        bar.style.top = distance + "px";
+        bar.style.height = 410 - e.clientY + sliderDistanceFromTop + "px";
         selected.style.opacity = userSelectedOpacity;
+        
+        axios.post("/switch-brightness", {
+                opacity: userSelectedOpacity - 0.2
+            }).then(data => {
+                console.log("data: ", data);
+            }).catch(err => console.log("err: ", err));
     }
-    
 }
 
 function selectBlock(e) {
@@ -38,5 +44,16 @@ function selectBlock(e) {
         block.style.opacity = 0.2;
     });
     e.target.style.opacity = userSelectedOpacity;
-    e.target.classList.add('selected');
+    e.target.classList.add("selected");
+
+    e.target.id === "off"
+        ? slider.classList.add("slider-off")
+        : slider.classList.remove("slider-off");
+
+    axios.post("/switch-colour", {
+            opacity: userSelectedOpacity - 0.2,
+            color: e.target.id
+        }).then(data => {
+            console.log("data: ", data);
+        }).catch(err => console.log("err: ", err));
 }
