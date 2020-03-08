74f891b9594db96fc30e99b5cbb4ae01973e25bf
Peter Anderson <pete.anderson@gmail.com>
2020-03-08 20:36:37 +0100

    test

--- main.py
+++ main.py
@@ -15,20 +15,6 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
-class Colours:
-    def __init__(self, r, g, b):
-        self.r = r
-        self.g = g
-        self.b = b
-
-diamond = Colours(69,172,165)
-gold = Colours(255,170,0)
-iron = Colours(173,140,118)
-lapis = Colours(13,42,151)
-red = Colours(204,0,0)
-
-print(diamond.r, diamond.g, diamond.b)
-
 # sanity check route
 @app.route('/ping', methods=['GET'])
 def ping_pong():
@@ -44,10 +30,39 @@ def home():
 def postmethod():
     post_data = request.get_json()
     color = post_data['color'];
-    print("post_data: ", color)
+    r = None
+    g = None
+    b = None
+
+    if color == 'diamond':
+        print("DIAMOND!")
+        r = 69
+        g = 172
+        b = 165
+    elif color == 'gold':
+        print("GOLD!")
+        r = 255
+        g = 170
+        b = 0
+    elif color == 'lapis':
+        print("lapis!")
+        r = 13
+        g = 42
+        b = 151
+    elif color == 'iron':
+        print("iron!")
+        r = 173
+        g = 140
+        b = 118
+    elif color == 'red':
+        print("red!")
+        r = 204
+        g = 0
+        b = 0
+    print("rgb", r, g, b)
     for x in range(8):
         for y in range(4):
-            uh.set_pixel(x, y, post_data['color'].r, post_data['color'].g, post_data['color'].b)
+            uh.set_pixel(x, y, r, g, b)
     uh.show()
     return jsonify({"success": "true"})
 
